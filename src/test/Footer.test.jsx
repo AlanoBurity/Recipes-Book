0475d21fcc1cda3f223fb0f4ea3655a89d0e72ed
Artur Vidor <vidorartur@gmail.com>
2022-08-22 15:28:32 -0300

    arrumando os commits

--- src/test/Footer.test.jsx
+++ src/test/Footer.test.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helper/renderWithRouter';
 import Footer from '../components/Footer';
-import Foods from '../pages/Foods';
 import App from '../App';
 
 describe('Testing Profile Page', () => {

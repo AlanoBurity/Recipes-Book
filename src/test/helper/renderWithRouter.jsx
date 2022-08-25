94bac202c879ef4348ddc37e46b261e0fe48e3ce
AlanoBurity <alanosantosdev@gmail.com>
2022-08-25 11:11:47 -0300

    requisito 62

--- src/test/helper/renderWithRouter.jsx
+++ src/test/helper/renderWithRouter.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import Provider from '../../context/Provider';
 
 const renderWithRouter = (component, historyEntries = ['/']) => {
   const history = createMemoryHistory({ initialEntries: historyEntries });
   return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(
+      <Provider>
+        <Router history={ history }>
+          {component}
+        </Router>
+      </Provider>,
+    ),
+    history,
   });
 };
 export default renderWithRouter;
